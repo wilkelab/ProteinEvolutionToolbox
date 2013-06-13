e6f48cc7ed3f6613d7e4800fc2d3cf552b684157
Austin Meyer <austin.g.meyer@gmail.com>
2013-06-12 23:43:33 -0500

    Finally cracked the robust algorithm.  Added my contact information.

--- python/translate_align_mapping.py
+++ python/translate_align_mapping.py
@@ -1,11 +1,19 @@
+###################################################################################
+## This script was written by Austin Meyer in the lab of Dr. Claus Wilke at the  ##
+## University of Texas at Austin.  For any questions or concerns you can email   ##
+## Austin at austin.g.meyer@gmail.com.                                           ##
+###################################################################################
+
+###################################################################################
 ## This script takes command line arguments in the following form                ##
 ## python translate_align_mapping.py fasta_file pdb_structure_file output_type   ##
-
+##                                                                               ##
 ## Here is an example run to output the aligned and reverse translated sequences ##
 ## python translate_align_mapping.py fasta.fasta structure.pdb seqs              ##
-
+##                                                                               ##
 ## Here is an example run to return the sequence-structure map                   ##
 ## python translate_align_mapping.py fasta.fasta structure.pdb map               ##
+###################################################################################
 
 ######################################################################
 ## Some of these includes are probably unecessary.                  ##
@@ -30,6 +38,17 @@ resdict = { 'ALA': 'A', 'CYS': 'C', 'ASP': 'D', 'GLU': 'E', 'PHE': 'F', \
             'GLY': 'G', 'HIS': 'H', 'ILE': 'I', 'LYS': 'K', 'LEU': 'L', \
             'MET': 'M', 'ASN': 'N', 'PRO': 'P', 'GLN': 'Q', 'ARG': 'R', \
             'SER': 'S', 'THR': 'T', 'VAL': 'V', 'TRP': 'W', 'TYR': 'Y' }
+            
+######################################################################
+## Here are the maximum RSA values per one letter amino acid        ##
+## abbreviation.                                                    ##
+######################################################################
+
+residue_max_acc = {'A': 113.0, 'R': 241.0, 'N': 158.0, 'D': 151.0, \
+                   'C': 140.0, 'Q': 189.0, 'E': 183.0, 'G': 85.0,  \
+                   'H': 194.0, 'I': 182.0, 'L': 180.0, 'K': 211.0, \
+                   'M': 204.0, 'F': 218.0, 'P': 143.0, 'S': 122.0, \
+                   'T': 146.0, 'W': 259.0, 'Y': 229.0, 'V': 160.0}
 
 ######################################################################
 ## This function accepts the full unfettered sequence file full pdb ##
@@ -173,10 +192,11 @@ def get_aa_fromPDB( pdb_id ):
 
 def translate_align_revtranslate( filename, pdb_id ):
     print("\n\nStarting Translate and Alignment Function\n\n")
+    
     input_handle1 = open(filename, 'rU')
     records1 = list(SeqIO.parse(input_handle1, "fasta"))
     input_handle1.close()
-	
+   
     proteinSequences = []
     MatchingIDs = []
     MatchingDict = {}
@@ -184,44 +204,54 @@ def translate_align_revtranslate( filename, pdb_id ):
     
     chain = get_aa_fromPDB( pdb_id )
     
-    for i in records1:	
+    for i in records1:  
         j = 0
         All_codons = []
         include_seq=True
-        #print i.id
         
         while j < len(i.seq)-2:
             codons = "%c%c%c" % (i.seq[j], i.seq[j+1], i.seq[j+2])
             element = [codons, translate(codons)]
             j+=3
             if element[1] == "*" and j > len(i.seq)-2:
-            	del element
-            	continue
+               del element
+               continue
+            elif element[1] == "*" or element[1] not in residue_max_acc:
+               include_seq=False
+               break
             All_codons.append(element)
             del element
+
         if include_seq:
-        	MatchingDict[i.id] = All_codons
-        	MatchingIDs.append(i.id)
-        	l+=1
+          MatchingDict[i.id] = All_codons
+          MatchingIDs.append(i.id)
+          l+=1
+          print l
+        else:
+          records1.remove(i)
+          continue
 
     first_line = "%c%s\n%s\n" % (">", "ref_seq", chain)
-
+    
     outfile2 = 'tmp_preAlign.txt'
+    
     output_handle2 = open(outfile2, 'w')
     output_handle2.write(first_line)
+
+    j = 0
     
     for an_id in MatchingIDs:
         seq = ""
         line_aa = ""
         for letter in range (0, len(MatchingDict[an_id]) ):
-        	seq = seq + (MatchingDict[an_id])[letter][1]
+         seq = seq + (MatchingDict[an_id])[letter][1]
         line_aa = "%c%s\n%s\n" % (">", an_id, seq)
         output_handle2.write(line_aa)
-        
+        j+=1
     output_handle2.close()
-
+    
     outfile3 = 'tmp_Aligned.txt'
-    command = "muscle -in " + outfile2 + " -clwstrict -out " + outfile3 + " -diags -maxiters 2 -sv -distance1 kbit20_3"
+    command = "mafft --clustalout " + outfile2 + " > " + outfile3
     subprocess.call(command, shell=True)
 	
     input_handle2 = open(outfile3, 'rU')
@@ -240,42 +270,41 @@ def translate_align_revtranslate( filename, pdb_id ):
         
       ### Make sure we're not working with the reference sequence
       this_sequence = (a_record.seq).tomutable()
-
+      
+      for a_codon in range( 0, len(ref_seq) ):
+        if a_record.seq[a_codon] == '-':
+          MatchingDict[a_record.id].insert( a_codon, ['---', '-'] )
+          
       seq_counter=0
       
       for a_codon in range( 0, len(ref_seq) ):
-        if a_record.seq[a_codon] == "-" and ref_seq[a_codon] != "-":
-          MatchingDict[a_record.id].insert( a_codon-seq_counter+1, ['---', '-'] )
-          continue
-        elif a_record.seq[a_codon] == "-" and ref_seq[a_codon] == "-":
-          this_sequence.pop(a_codon-seq_counter)
+        if ref_seq[a_codon] == "-":
+          dict_codon = MatchingDict[a_record.id].pop( a_codon - seq_counter )
           seq_counter+=1
-          continue
-        elif ref_seq[a_codon]=="-" and a_record.seq[a_codon]!="-":
-          seq_codon = str(this_sequence.pop( a_codon-seq_counter ))
-          dict_codon = MatchingDict[a_record.id].pop( a_codon-seq_counter )
-          assert seq_codon == dict_codon[1], \
-            "\nThis is the dictionary codon: %s This is the sequence codon: %s\nHere is the seq id: %s Here is the location in the sequence: %s" \
-            % (dict_codon, seq_codon, a_record.id, a_codon-seq_counter)
-          seq_counter+=1
-          continue
-        else:
-          continue
-
-      assert len(this_sequence) == len(MatchingDict[a_record.id]), "\nLength mod seq: %s Length of Ref Seq: %s\n" \
-        % (len(this_sequence), len(MatchingDict[a_record.id]))
-            
-      a_record.seq=this_sequence.toseq()
+          
+      assert len(remove_values_from_list(ref_seq, "-")) == len(MatchingDict[a_record.id]), "\nLength mod seq: %s Length of Ref Seq: %s\n" \
+        % (len(remove_values_from_list(ref_seq, "-")), len(MatchingDict[a_record.id]))
     				
     ref_seq = remove_values_from_list(ref_seq, "-")
     
     return(ref_seq, aligned_records, MatchingDict)
-    
+
+######################################################################
+## This function is for outputting the final aligned nucleotide     ##
+## sequences.  It also outputs the corresponding nucleotide file    ##
+## temporarily for error checking.  If you want to preserve the tmp ##
+## files you must comment out the second to last line of the        ##
+## function.                                                        ##
+######################################################################
+
 def output_nucleotide_sequences(ref_seq, aligned_records, MatchingDict):
 
   outfile = 'Final_Aligned.txt'
+  aa_outfile = 'tmp_aa_out.txt'
+
   output_handle = open(outfile, 'w')
-  
+  aa_out_handle = open(aa_outfile, 'w')
+
   for a_record in aligned_records:
  
     seq = ""
@@ -285,13 +314,23 @@ def output_nucleotide_sequences(ref_seq, aligned_records, MatchingDict):
     for j in range(0, len(ref_seq)):
       assert ref_seq[j]!="-"
       nuc_seq = nuc_seq + (MatchingDict[a_record.id])[j][0]
+      aa_seq  = aa_seq  + (MatchingDict[a_record.id])[j][1]
     
     line = "%c%s\n%s\n" % (">", a_record.id, nuc_seq)
     output_handle.write(line)
 
+    line = "%c%s\n%s\n" % (">", a_record.id, aa_seq)
+    aa_out_handle.write(line)
+
   print("\n\nThe output file is name: " + outfile + "\n")
   subprocess.call('rm tmp_*', shell=True)
   return 0
+
+######################################################################
+## This function outputs a numerical map for each input nucleotide  ##
+## sequence.  The sequence of comma delimited numbers provides the  ##
+## corresponding site in the pdb's amino acid sequence.             ##
+######################################################################
 
 def output_numerical_map(ref_seq, aligned_records, MatchingDict):
 
